fd330f634236473e8e4e00016b7995061540298e
Christian Walker <christiandwalker2021@gmail.com>
2024-11-09 00:30:06 -0600

    ui updates

--- apps/web/src/components/landing/Hero.tsx
+++ apps/web/src/components/landing/Hero.tsx
@@ -11,8 +11,8 @@ export default function Hero() {
 				<div className="absolute left-[20%] top-[-30vh] h-[110vh] w-[225px] -translate-x-5 -rotate-[50deg] bg-white opacity-20 blur-3xl"></div>
 				<div className="relative">
 					<div className="absolute h-full w-full rounded-3xl bg-hackathon opacity-10 blur-3xl"></div>
-					<div className="z-10 grid h-min grid-cols-1 items-center gap-4 md:grid-cols-2 md:gap-0">
-						<div className="relative h-[250px] w-[250px] md:h-[400px] md:w-[400px]">
+					<div className="z-10 flex h-min flex-col items-center justify-center gap-4 md:flex-row">
+						<div className="relative mx-auto h-[250px] w-[250px] md:h-[400px] md:w-[400px]">
 							<Image
 								src="/img/logo/rowdyconlogo.png"
 								alt="RowdyCon Logo"
@@ -20,7 +20,7 @@ export default function Hero() {
 								className="object-contain"
 							/>
 						</div>
-						<div className="z-10 flex flex-col items-center space-y-2 text-center md:items-start md:space-y-3 md:text-left">
+						<div className="z-10 flex flex-col items-center text-center md:items-start md:space-y-3 md:text-left">
 							<h1 className="text-4xl font-black text-hackathon dark:bg-gradient-to-tl dark:from-hackathon/80 dark:to-white dark:bg-clip-text dark:text-transparent md:text-7xl">
 								<span style={{ color: "#ff6347" }}>
 									Nov. 16th
@@ -34,21 +34,14 @@ export default function Hero() {
 									className="object-contain"
 								/>
 							</div>
-							<p className="md:text-md text-lg font-bold text-muted-foreground">
-								<span
-									style={{
-										fontSize: "2rem",
-										color: "#ff6347",
-									}}
-								>
+							<p className="text-xl md:text-4xl text-[#ff6347] font-bold text-muted-foreground">
 									@ UTSA San Pedro I and Online
-								</span>
 							</p>
 						</div>
 					</div>
 				</div>
 				{/*old, change it to this when removing the CFW button after it closes: mt-2 flex w-full items-center justify-center */}
-				<div className="mt-2 flex flex-col w-full items-center justify-center space-y-4">
+				<div className="mt-4 md:mt-0 flex w-full flex-col items-center justify-center space-y-4">
 					{" "}
 					{/* Reduced margin here */}
 					<Suspense
@@ -67,8 +60,6 @@ export default function Hero() {
 					>
 						<LandingButton />
 					</Suspense>
-
-
 					{/* CFW Button. Remove when closed. */}
 					<Link href={"https://forms.gle/VFUZ25dLLn9UsWkt6"}>
 						<Button
--- apps/web/src/components/landing/LandingButton.tsx
+++ apps/web/src/components/landing/LandingButton.tsx
@@ -7,16 +7,30 @@ export default async function LandingButton(){
 
   if (!userId){
     return (
-		<Link href={"/register"}>
-			<Button
-				variant={"outline"}
-				size={"lg"}
-				className="px-10 py-5 text-xl md:px-20 md:py-10 md:text-2xl"
-				style={{ backgroundColor: "#ff6347" }}
-			>
-				Register Now!
-			</Button>
-		</Link>
+		<div className="grid grid-cols-1">
+			<div className="flex h-full w-full flex-col items-center gap-x-4 space-y-2 md:space-y-0 md:flex-row">
+				<Link href={"/register"}>
+					<Button
+						variant={"outline"}
+						size={"lg"}
+						className="px-10 py-5 text-xl md:px-20 md:py-10 md:text-2xl"
+						style={{ backgroundColor: "#ff6347" }}
+					>
+						Register Now!
+					</Button>
+				</Link>
+				<Link href={"/sign-in"} className="w-full md:w-auto">
+					<Button
+						variant={"outline"}
+						size={"lg"}
+						className="px-10 py-5 text-xl md:px-20 md:py-10 md:text-2xl w-full"
+						style={{ backgroundColor: "#ff6347" }}
+					>
+						Sign In
+					</Button>
+				</Link>
+			</div>
+		</div>
 	);
   }
 
